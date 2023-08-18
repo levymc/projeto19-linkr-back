6e515092a4bfeb3f8ad66ee0acea87b48495f302
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-08-18 17:38:41 -0300

    Added updateHashtagsRepository

--- src/repositories/hashtag.repository.js
+++ src/repositories/hashtag.repository.js
@@ -26,3 +26,42 @@ export async function getHashtagRepository(hashtag) {
     }
 
 }
+
+export async function updateHashtagsRepository(postId, ...hashtags) {
+
+    try {
+
+        let queryValues;
+        hashtags.forEach(async (hashtag) => { queryValues += `('${hashtag}'), ` });
+        queryValues = queryValues.substring(0, str.length - 2);
+
+        const dbResponse = await db.query(`
+            INSERT INTO "hashtag" ("hashtagName")
+                VALUES ${queryValues}
+            ON CONFLICT ("hashtagName") DO UPDATE
+                SET "currentInteractions" = "currentInteractions" + 1
+            RETURNING "hashtagId", "hashtagName";
+        `)
+
+        const hashtagIdArray = dbResponse.rows.map((e) => { e.hashtagId });
+
+        let queryJunctionValues;
+        hashtagIdArray.forEach(async (hashtagId) => { queryJunctionValues += `('${hashtagId}', '${postId}'), ` });
+        queryJunctionValues = queryValues.substring(0, str.length - 2);
+
+        await db.query(`
+            INSERT
+                INTO "hashtagPostJunction" ("hashtagId", "postId")
+                VALUES ${queryJunctionValues};
+        `);
+
+        console.log(dbResponse.rows);
+        return dbResponse.rows;
+
+    } catch (error) {
+
+        throw new Error(error.message);
+
+    }
+
+}
