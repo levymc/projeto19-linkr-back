c966fbf82e023cbb97ab8dd63130816699787f69
João Vitor Diniz Mistrinel <abissaldev@gmail.com>
2023-08-18 14:49:17 -0300

    add users search

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,18 +1,22 @@
 import { Router } from "express";
-import { newPost, getPosts, editPosts } from "../controllers/post.controller.js";
+import {
+  newPost,
+  getPosts,
+  editPosts,
+} from "../controllers/post.controller.js";
 
-import userRouter from "./auth.routes.js";
+import authRouter from "./auth.routes.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { postSchema } from "../schemas/postSchemas.js";
+import usersRouter from "./users.routes.js";
 
 const router = Router();
 
+router.post("/newPost", newPost);
+router.get("/posts", getPosts);
+router.put("/posts", validateSchema(postSchema), editPosts);
 
-router.post('/newPost', newPost)
-router.get('/posts', getPosts)
-router.put('/posts', validateSchema(postSchema), editPosts)
-
-
-router.use(userRouter);
+router.use(authRouter);
+router.use(usersRouter);
 
 export default router;
