651ac258b3ba12cd91bc804ae8286dfd0a905d51
samufigh <samufigh21@gmail.com>
2023-08-19 14:56:53 -0300

    fix: userId

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -2,7 +2,7 @@ import { db } from "../database/db.connection.js";
 
 export async function getUserByName(letter) {
   const result = await db.query(
-    'SELECT "name", "imageUrl" FROM "users" WHERE LOWER("name") LIKE $1',
+    'SELECT "name", "imageUrl", "userId" FROM "users" WHERE LOWER("name") LIKE $1',
     [`%${letter.toLowerCase()}%`]
   );
   return result.rows;
