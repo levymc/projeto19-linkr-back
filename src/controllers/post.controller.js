863b98b8719b1203978427b32fcb56817cb700fa
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-08-20 15:07:31 -0300

    Fixed formatting of post.controller

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -12,28 +12,28 @@ dayjs.extend(timezone)
 const timezoneName = 'America/Sao_Paulo';
 
 
-export async function newPost(req, res){
+export async function newPost(req, res) {
     req.body.userId = 1
     const timestampAtual = Date.now();
     console.log(req.body.metadata['og:image']);
-    try{
+    try {
         const insertedPost = await postRepo.createPost(
-                                                        res.locals.userId, 
-                                                        req.body.content, 
-                                                        req.body.postUrl,
-                                                        req.body.metadata['og:image'],
-                                                        req.body.metadata['og:title'],
-                                                        req.body.metadata['og:description']
-                                                    )
+            res.locals.userId,
+            req.body.content,
+            req.body.postUrl,
+            req.body.metadata['og:image'],
+            req.body.metadata['og:title'],
+            req.body.metadata['og:description']
+        )
         if (insertedPost) return res.status(201).send(insertedPost)
-    }catch(err){
-        console.error("Erro na criação de Post: ",err)
+    } catch (err) {
+        console.error("Erro na criação de Post: ", err)
         res.status(500).send("Erro na criação de Post: " + err)
     }
 }
 
-export async function getPosts (req, res) {
-    try{
+export async function getPosts(req, res) {
+    try {
         const posts = await postRepo.getPosts()
         // const user = await getUserById(res.locals.userId)
         const response = {
@@ -41,44 +41,44 @@ export async function getPosts (req, res) {
             // user: user
         }
         if (posts) return res.status(200).send(response)
-    }catch(err){
-        console.error("Erro no getPosts: ",err)
+    } catch (err) {
+        console.error("Erro no getPosts: ", err)
         res.status(500).send("Erro no getPosts: " + err)
     }
 }
 
-export async function getUserPosts (req, res) {
-    try{
+export async function getUserPosts(req, res) {
+    try {
         const user = await getUserById(req.params.id)
         const posts = await postRepo.getUserPosts(req.params.id)
-        const response = {user: user, posts: posts}
+        const response = { user: user, posts: posts }
 
         if (posts) return res.status(200).send(response)
-    }catch(err){
-        console.error("Erro no getPosts: ",err)
+    } catch (err) {
+        console.error("Erro no getPosts: ", err)
         res.status(500).send("Erro no getPosts: " + err)
     }
 }
 
-export async function editPosts (req, res) {
-    const {text, hashtags, postId} = req.body
+export async function editPosts(req, res) {
+    const { text, hashtags, postId } = req.body
     const editedAt = dayjs().format('YYYY-MM-DD HH:mm:ssZ');
-    try{
+    try {
         const updatePost = await postRepo.atualizarPost(postId, text, hashtags, editedAt)
-        if(updatePost) return res.sendStatus(200)
-    }catch(err){
+        if (updatePost) return res.sendStatus(200)
+    } catch (err) {
         res.status(500).send(err)
     }
 }
 
-export async function deletePost(req, res){
-    const {id} = req.params
-    try{
+export async function deletePost(req, res) {
+    const { id } = req.params
+    try {
         const post = await postRepo.sendPost(id);
-        if(!post) return res.sendStatus(404);
+        if (!post) return res.sendStatus(404);
         const delPost = await postRepo.deletarPost(id)
-        if(delPost) return res.sendStatus(200)
-    }catch(err){
+        if (delPost) return res.sendStatus(200)
+    } catch (err) {
         res.status(500).send(err)
     }
 }
